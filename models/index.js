3edacd26269be0850e9e1272bcc54d95465be758
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2026-08-12 19:05:32 +0700

    add Message model import to index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const sequelize = require('../config/database');
 const Admin = require('./admin.model');
 const Product = require('./product.model');
+const Message = require('./message.model');
 
 module.exports = {
   sequelize,
